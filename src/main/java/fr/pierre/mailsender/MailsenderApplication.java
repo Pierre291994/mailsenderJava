f1b51134b223d084c851fdf8f67de2c73ccea0b4
pierre <vigouroux001@gmail.com>
2023-04-02 13:18:10 +0200

    init

---
+++ src/main/java/fr/pierre/mailsender/MailsenderApplication.java
@@ -0,0 +1,13 @@
+package fr.pierre.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderApplication.class, args);
+	}
+
+}
